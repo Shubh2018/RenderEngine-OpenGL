94760b5ecbd736131880484d9236693c90054f68
Shubh2018 <shubhendrachaddha.16@gmail.com>
2026-02-25 22:06:55 +0100

    Switched to 3D

--- main/main.cpp
+++ main/main.cpp
@@ -39,10 +39,6 @@ int main()
     glUniform1i(glGetUniformLocation(shader->Handle(), "tex1"), 0);
     glUniform1i(glGetUniformLocation(shader->Handle(), "tex2"), 1);
 
-    glm::vec4 vec(1.f, 0.f, 0.f, 1.f);
-
-    unsigned int transformLoc = glGetUniformLocation(shader->Handle(), "transform");
-
     while (!window->WindowShouldClose())
     {
         processInput(window->GetWindow());
@@ -50,16 +46,24 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glm::mat4 model(1.f);
+        model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.f, 0.f, 0.f));
+
+        glm::mat4 view(1.f);
+        view = glm::translate(view, glm::vec3(0.f, 0.f, -3.f));
+
+        glm::mat4 projection(1.f);
+        projection = glm::perspective(glm::radians(45.0f), 800.f / 600.f, 0.1f, 100.0f);
+
+        int modelLocation = glGetUniformLocation(shader->Handle(), "model");
+        int viewLocation = glGetUniformLocation(shader->Handle(), "view");
+        int projectionLocation = glGetUniformLocation(shader->Handle(), "projection");
+
+        glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(model));
+        glUniformMatrix4fv(viewLocation, 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(projectionLocation, 1, GL_FALSE, glm::value_ptr(projection));
+
         shader->Use();
-
-        glm::mat4 trans(1.f);
-
-        trans = glm::translate(trans, glm::vec3(.75f, .75f, 0.0f));
-        trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.f, 0.f, 1.f));
-        trans = glm::scale(trans, glm::vec3(.5f, .5f, .5f));
-        vec = trans * vec;
-
-        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
 
         texture1->ActiveTexture(GL_TEXTURE0);
         texture2->ActiveTexture(GL_TEXTURE1);
