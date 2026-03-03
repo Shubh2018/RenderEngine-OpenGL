e9ae0bff578950c9b8dc8bf2e12fb1e1ffdcda7c
Shubh2018 <shubhendrachaddha.16@gmail.com>
2026-03-03 21:32:16 +0100

    Camera Movement - Rotation pending

--- main/main.cpp
+++ main/main.cpp
@@ -19,6 +19,13 @@ void processInput(GLFWwindow *window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+glm::vec3 cameraPos = glm::vec3(0.f, 0.f, 3.f);
+glm::vec3 cameraFront = glm::vec3(0.f, 0.f, -1.f);
+glm::vec3 cameraUp = glm::vec3(0.f, 1.f, 0.f);
+
+float deltaTime = 0.f;
+float lastFrame = 0.f;
+
 int main()
 {
     std::unique_ptr<Window> window = std::make_unique<Window>("OpenGL", SCR_WIDTH, SCR_HEIGHT);
@@ -41,23 +48,34 @@ int main()
 
     glEnable(GL_DEPTH_TEST);
 
+    // glm::vec3 cameraTarget = glm::vec3(0.f, 0.f, 0.f);
+    // glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
+
+    // glm::vec3 up = glm::vec3(0.f, 1.f, 0.f);
+    // glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
+
+    // glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight);
+
     while (!window->WindowShouldClose())
     {
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
         processInput(window->GetWindow());
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glm::mat4 model(1.f);
-        model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(1.f, 0.f, 0.f));
+        //model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(1.f, 0.f, 0.f));
         model = glm::scale(model, glm::vec3(0.5f, 0.5f, 0.5f));
 
         glm::mat4 view(1.f);
-        view = glm::translate(view, glm::vec3(0.f, 0.f, -3.f));
+        view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
 
         glm::mat4 projection(1.f);
         projection = glm::perspective(glm::radians(45.0f), 800.f / 600.f, 0.1f, 100.0f);
-
 
         int modelLocation = glGetUniformLocation(shader->Handle(), "model");
         int viewLocation = glGetUniformLocation(shader->Handle(), "view");
@@ -84,8 +102,18 @@ int main()
 
 void processInput(GLFWwindow *window)
 {
+    const float cameraSpeed = 2.5f * deltaTime;
+
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        cameraPos += cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        cameraPos -= cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        cameraPos += glm::cross(cameraUp, cameraFront) * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        cameraPos -= glm::cross(cameraUp, cameraFront) * cameraSpeed;
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
